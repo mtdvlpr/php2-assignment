c5ff4eb70864862ba496144a61ea72a88977f199
Manoah <149895ja@gmail.com>
2021-06-16 14:22:31 +0200

    fds

--- public/api/orderWebhook.php
+++ public/api/orderWebhook.php
@@ -3,36 +3,36 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../model/PDF.php';
 require_once __DIR__ . '/../../model/mailer.php';
 require_once __DIR__ . '/../../controller/paymentController.php';
-
+/*
 $mailer = new Mailer();
 $mailer->sendMail(
   subject: "test",
   body: "Page visited",
   address: "149895ja@gmail.com"
-);
+);*/
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
   $paymentController = new PaymentController();
   $mollieId = $_POST['id'] ?? '';
-
+/*
   $mailer = new Mailer();
   $mailer->sendMail(
     subject: "test",
     body: "MollieID: " + $mollieId,
     address: "149895ja@gmail.com"
-  );
+  );*/
 
   if (!empty($mollieId))
   {
     $status = $paymentController->getMollieOrderStatus($mollieId);
     $paymentController->setOrderStatus($mollieId, $status);
 
-    $mailer->sendMail(
+    /*$mailer->sendMail(
       subject: "test",
       body: "Status: " + $status,
       address: "149895ja@gmail.com"
-    );
+    );*/
 
     if ($status == "paid")
     {
