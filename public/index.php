77aa04630a36178d34570afc08b9553a9cbaade3
Manoah <149895ja@gmail.com>
2021-12-28 22:47:27 +0100

    chore: remove debug code

--- public/index.php
+++ public/index.php
@@ -1,9 +1,4 @@
 <?php
   require_once '../view/router.php';
-
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
-
   $router = new Router();
   $router->handleRoute();
